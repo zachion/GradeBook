2d451d1402e058783e834c6491aacbd7e0ebf6a8
Zach Ioannou <zioannou77@gmail.com>
2018-10-21 19:44:51 +0300

    added unit tests

---
+++ Grade.Tests/GradesTest.cs
@@ -0,0 +1,59 @@
+﻿using Grades;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Tests
+{
+    [TestClass]
+    public class GradesTest
+    {
+        [TestMethod]
+        public void ComputeHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrades(5);
+            book.AddGrades(4);
+            book.AddGrades(3.4f);
+
+            GradeStatistics Stats = book.ComputeStats();
+
+            Assert.AreEqual(5, Stats.HighestGrade);
+        }
+
+        [TestMethod]
+        public void ComputeLowestGrade()
+        {
+            GradeBook Book = new GradeBook();
+
+            Book.AddGrades(5);
+            Book.AddGrades(4);
+            Book.AddGrades(3.4f);
+
+            GradeStatistics Stats = Book.ComputeStats();
+
+            Assert.AreEqual(3.4f, Stats.LowestGrade);
+        }
+
+        [TestMethod]
+        public void ComputeAverageGrade()
+        {
+            GradeBook Book = new GradeBook();
+
+            Book.AddGrades(5);
+            Book.AddGrades(4);
+            Book.AddGrades(3.4f);
+
+            GradeStatistics Stats = Book.ComputeStats();
+
+            Assert.AreEqual(4.13f, Stats.AverageGrade, 0.01);
+        }
+
+    }
+
+    
+
+}
---
+++ Grade.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Grades.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Grades.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b96e0f29-5d6f-477f-ae5c-bbe1d3e8b391")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
