b9aed8299cb06757e4a55d231f091deaaf9e22ed
Zach Ioannou <zioannou77@gmail.com>
2018-10-21 14:20:19 +0300

    commit grade book

---
+++ GradeBook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeBook
+    {
+
+        List<float> Grades = new List<float>();
+
+        public GradeStatistics ComputeStats()
+        {
+
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+
+            foreach (float grade in Grades)
+            {
+                sum += grade;
+
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+            }
+
+            stats.AverageGrade = sum / Grades.Count();
+
+            return stats;
+        }
+
+
+
+        public void AddGrades(float grade)
+        {
+            Grades.Add(grade);
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook gradeBook = new GradeBook();
+
+            gradeBook.AddGrades(5);
+            gradeBook.AddGrades(4);
+            gradeBook.AddGrades(3.4f);
+
+            GradeStatistics gradeStatistics = gradeBook.ComputeStats();
+
+            Console.WriteLine(gradeStatistics.AverageGrade);
+            Console.WriteLine(gradeStatistics.HighestGrade);
+            Console.WriteLine(gradeStatistics.LowestGrade);
+
+        }
+    }
+}
